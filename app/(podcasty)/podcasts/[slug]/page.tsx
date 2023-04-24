0a84ca4fcb9e6540329e2326544aa9e0b37db0ed
Rajender <rajenderkatkuri123@gmail.com>
2023-04-24 23:00:04 +0530

    Ref Footer

--- app/(podcasty)/podcasts/[slug]/page.tsx
+++ app/(podcasty)/podcasts/[slug]/page.tsx
@@ -41,34 +41,36 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
 
   return (
     <div className="body-font font-mabry">
-      <p className="text-center my-4 text-4xl font-bold">{title}</p>
-      <div className="mb-5 flex bg-[#ffffff] flex-col justify-center lg:flex lg:flex-row lg:justify-evenly lg:items-center h-[480px]">
-        <div className="border-2 border-black rounded-lg text-center w-full p-3 lg:mr-3 hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:shadow-lg h-[480px] hover:border-r-4 hover:border-b-4 hover:border-black">
-          <div className="w-full relative rounded-lg h-[85%]">
+      <p className="text-center lg:my-4 lg:text-4xl sm:text-2xl font-bold">
+        {title}
+      </p>
+      <div className="lg:mb-5 flex bg-[#f4f4f1] flex-col justify-center lg:flex lg:flex-row lg:justify-evenly lg:items-center h-[480px]">
+        <div className="border-2 sm:mb-2 bg-[#ffffff] border-black rounded-lg text-center w-full p-3 mr-3 hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:shadow-lg h-[480px] hover:border-r-4 hover:border-b-4 hover:border-black">
+          <div className="w-full relative rounded-lg lg:h-[87.5%] sm:h-[71.25%]">
             <Image className="rounded-lg" src={bannerURL} alt={title} fill />
           </div>
           <div className="w-full flex justify-center mt-2">
             <audio src={audioURL} controls />
           </div>
         </div>
-        <div className="border-2 border-black rounded-lg w-[50%] flex flex-col h-full p-3 bg-white hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:border-r-4 hover:border-b-4 hover:border-black">
+        <div className="border-2 border-black rounded-lg lg:w-[50%] flex lg:flex-col sm:flex-col sm:p-2 sm:pb-2 h-full lg:p-3 bg-white hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:border-r-4 hover:border-b-4 hover:border-black">
           <div>
-            <p className="pb-1 text-xl">By: {author}</p>
-            <p className="pb-1 text-xl">
+            <p className="pb-1 text-xl">Author: {author}</p>
+            <p className="pb-1">
               Category:{" "}
               {categories.map((category, index) => (
                 <span key={index}>{category}</span>
               ))}
             </p>
-            <p className="pb-1 text-xl">
+            <p className="pb-1">
               Favorites: {favorites ?? <span>loading</span>}
             </p>
           </div>
-          <div className="flex-col my-3 min-h-[285px] max-h-[285px]">
+          <div className="lg:flex-col my-3 lg:min-h-[285px] lg:max-h-[285px]">
             <p className="text-xl font-semibold">Description</p>
             <p className="bg-white mt-2">{desc}</p>
           </div>
-          <div className="my-4 mx-auto">
+          <div className="lg:my-4 sm:my-2 mx-auto">
             <SignedIn>
               {favoriteByUser > 0 && (
                 /* @ts-ignore */
@@ -85,15 +87,15 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
           </SignedOut>
         </div>
       </div>
-      <div className="flex flex-row justify-center sm:flex-col sm:mt-2">
-        <div className="sm:flex sm:flex-row sm:justify-between sm: w-auto">
-          <button className="p-2 mt-2 mr-1 outline rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
+      <div className="flex justify-center flex-col mt-2">
+        <div className="flex flex-row justify-between w-auto">
+          <button className="p-2 mt-2 mr-1 outline w-[50%] rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
             More from{" "}
             <b>
               <i>{author}</i>
             </b>
           </button>
-          <button className="p-2 mt-2 ml-1 outline rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
+          <button className="p-2 mt-2 ml-1 outline w-[50%] rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
             More from{" "}
             {categories.map((category, index) => (
               <span key={index}>
@@ -104,7 +106,7 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
             ))}
           </button>
         </div>
-        <div className="sm:flex sm:flex-col mb-5">
+        <div className="flex flex-col mb-5">
           <Link
             href="/"
             className="text-center p-2 mt-2 border-2 rounded-lg bg-[#000000] text-white hover:bg-[#FF90E7] hover:text-black hover:border-black hover:border-b-4 hover:border-r-4"
