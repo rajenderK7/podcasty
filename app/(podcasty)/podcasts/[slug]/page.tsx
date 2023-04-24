049008bda52e26af8e3f7ed48fe070173f5262cc
Rajender <rajenderkatkuri123@gmail.com>
2023-04-24 16:38:32 +0530

    Style Card component

--- app/(podcasty)/podcasts/[slug]/page.tsx
+++ app/(podcasty)/podcasts/[slug]/page.tsx
@@ -38,18 +38,17 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
     await fetchPodcast(slug);
   const favorites = await fetchFavoriteCount(_id);
   const favoriteByUser = await fetchFavoritedByUser(userId!, _id);
-  console.log(favoriteByUser);
 
   return (
     <div className="body-font font-mabry">
       <p className="text-center my-4 text-4xl font-bold">{title}</p>
       <div className="mb-5 flex bg-[#ffffff] flex-col justify-center lg:flex lg:flex-row lg:justify-evenly lg:items-center h-[480px]">
         <div className="border-2 border-black rounded-lg text-center w-full p-3 lg:mr-3 hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:shadow-lg h-[480px] hover:border-r-4 hover:border-b-4 hover:border-black">
-          <div className="w-full relative rounded-lg h-[90%]">
-            <Image className="rounded-lg" src={bannerURL} alt={title} fill/>
+          <div className="w-full relative rounded-lg h-[85%]">
+            <Image className="rounded-lg" src={bannerURL} alt={title} fill />
           </div>
-          <div className="w-full flex justify-center">
-            <audio src={audioURL} controls/>
+          <div className="w-full flex justify-center mt-2">
+            <audio src={audioURL} controls />
           </div>
         </div>
         <div className="border-2 border-black rounded-lg w-[50%] flex flex-col h-full p-3 bg-white hover:-translate-x-1 hover:-translate-y-1 duration-200 hover:border-r-4 hover:border-b-4 hover:border-black">
@@ -61,13 +60,13 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
                 <span key={index}>{category}</span>
               ))}
             </p>
-            <p className="pb-1">Favorites: {favorites ?? <span>loading</span>}</p>
+            <p className="pb-1">
+              Favorites: {favorites ?? <span>loading</span>}
+            </p>
           </div>
           <div className="flex-col my-3 min-h-[285px] max-h-[285px]">
             <p className="text-xl font-semibold">Description</p>
-            <p className="p-2 bg-white mt-2">
-              {desc}
-            </p>
+            <p className="bg-white mt-2">{desc}</p>
           </div>
           <div className="my-4 mx-auto">
             <SignedIn>
@@ -89,18 +88,28 @@ const PodcastPage = async ({ params: { slug } }: Slug) => {
       <div className="flex flex-row justify-center sm:flex-col sm:mt-2">
         <div className="sm:flex sm:flex-row sm:justify-between sm: w-auto">
           <button className="p-2 mt-2 mr-1 outline rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
-            More from <b><i>{author}</i></b>
+            More from{" "}
+            <b>
+              <i>{author}</i>
+            </b>
           </button>
           <button className="p-2 mt-2 ml-1 outline rounded-lg bg-[#f3a642] hover:bg-[#77c9ed] hover:border-black hover:border-b-4 hover:border-r-4 sm:w-1/2">
-            More from {" "}
+            More from{" "}
             {categories.map((category, index) => (
-              <span key={index}><b><i>{category}</i></b></span>
+              <span key={index}>
+                <b>
+                  <i>{category}</i>
+                </b>
+              </span>
             ))}
           </button>
         </div>
-        <div className="sm:flex sm:flex-col">
-          <Link href="/" className="text-center p-2 mt-2 border-2 rounded-lg bg-[#000000] text-white hover:bg-[#FF90E7] hover:text-black hover:border-black hover:border-b-4 hover:border-r-4">
-            HOME
+        <div className="sm:flex sm:flex-col mb-5">
+          <Link
+            href="/"
+            className="text-center p-2 mt-2 border-2 rounded-lg bg-[#000000] text-white hover:bg-[#FF90E7] hover:text-black hover:border-black hover:border-b-4 hover:border-r-4"
+          >
+            Home
           </Link>
         </div>
       </div>
--- components/Card.tsx
+++ components/Card.tsx
@@ -13,7 +13,10 @@ export interface CardProps {
 function Card({ title, author, desc, _slug, bannerURL }: CardProps) {
   return (
     <div className="min-w-[320px] max-w-[320px]  bg-white flex flex-col m-2 border-2 border-gray-400 rounded-lg shadow">
-      <Link href={`/podcasts/${_slug}`} className="w-full relative rounded-lg min-h-[200px] max-h-[200px]">
+      <Link
+        href={`/podcasts/${_slug}`}
+        className="w-full relative rounded-lg min-h-[200px] max-h-[200px]"
+      >
         <Image fill className="rounded-t-lg" src={bannerURL} alt={title} />
       </Link>
       <div className=" border-t border-black p-3 flex flex-col">
@@ -33,7 +36,7 @@ function Card({ title, author, desc, _slug, bannerURL }: CardProps) {
           </p>
         </div>
 
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between items-center  ">
           <Link
             href={`/podcasts/${_slug}`}
             type="button"
@@ -41,24 +44,7 @@ function Card({ title, author, desc, _slug, bannerURL }: CardProps) {
           >
             <p>Listen</p>
           </Link>
-          <button
-            type="button"
-            className="text-white focus:ring-2 focus:outline-none focus:ring-gray-300 font-medium rounded-lg border border-black text-sm px-1 py-1 mr-2 mb-2 hover:bg-[#77c9ed] hover:text-black hover:border-black hover:border-b-2 hover:border-r-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="red-500"
-              className="w-6 h-6"
-            >
-              <path
-                className="fill-red-500"
-                stroke-linecap="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-              />
-            </svg>
-          </button>
+          <p className="font-bold">8.9K Favorites</p>
         </div>
       </div>
     </div>
